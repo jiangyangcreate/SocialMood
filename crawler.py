6482ba1c8d686cb30302d63c85eb35df1053f78c
jiangyangcreate <jiangyangcreate@gmail.com>
2024-10-22 11:24:45 +0800

    💄

--- crawler.py
+++ crawler.py
@@ -62,26 +62,26 @@ class Qwen2Model:
         return float(response)
 
 class ChartGenerator:
-    def create_bar_chart(self, data , y_label="Sentiment",title="Sentiment Scores by Source") -> Bar:
+    def create_bar_chart(self, data , y_label="Sentiment",title="正负面情绪") -> Bar:
         """Generate a bar chart with positive, negative, and absolute sentiment scores."""
 
         x_data, y_data_positive, y_data_negative, y_data_absolute = zip(*data)
         bar = (
             Bar()
             .add_xaxis(x_data)
-            .add_yaxis("Positive Sentiment", y_data_positive, stack="stack1", category_gap="50%")
-            .add_yaxis("Negative Sentiment", y_data_negative, stack="stack2", category_gap="50%")
-            .add_yaxis("Absolute Sentiment", y_data_absolute, stack="stack3", category_gap="50%")
+            .add_yaxis("积极", y_data_positive, stack="stack1", category_gap="50%")
+            .add_yaxis("消极", y_data_negative, stack="stack2", category_gap="50%")
+            .add_yaxis("绝对值", y_data_absolute, stack="stack3", category_gap="50%")
             .set_global_opts(
                 title_opts=opts.TitleOpts(title=title, pos_left="center"),
-                xaxis_opts=opts.AxisOpts(name="Source"),
-                yaxis_opts=opts.AxisOpts(name="Sentiment Score"),
+                xaxis_opts=opts.AxisOpts(name="来源"),
+                yaxis_opts=opts.AxisOpts(name="情绪得分"),
                 legend_opts=opts.LegendOpts(pos_top="10%", pos_right="10%"),
             )
         )
         return bar
 
-    def create_line_chart(self,data,y_label="Weighted Sentiment Score Sum",title="Sum of Weighted Sentiment Scores Over Time") -> Line:
+    def create_line_chart(self,data,y_label="情绪得分",title="绝对情绪变化折线图") -> Line:
         """Generate a line chart."""
         x_data, y_data = zip(*data) 
         line = (
@@ -90,14 +90,13 @@ class ChartGenerator:
             .add_yaxis(y_label, y_data, is_smooth=True)
             .set_global_opts(
                 title_opts=opts.TitleOpts(title=title, pos_left="center"),
-                xaxis_opts=opts.AxisOpts(name="Date", axislabel_opts=opts.LabelOpts(rotate=45)),
-                yaxis_opts=opts.AxisOpts(name=y_label),
-                legend_opts=opts.LegendOpts(pos_bottom="0%")
+                xaxis_opts=opts.AxisOpts(name="日期"),
+                legend_opts=opts.LegendOpts(is_show=False)
             )
         )
         return line
 
-    def create_pie_chart(self, data_pairs, title="Information Source Heat") -> Pie:
+    def create_pie_chart(self, data_pairs, title="信息来源占比") -> Pie:
         """Generate a pie chart."""
         pie = (
             Pie()
@@ -110,7 +109,7 @@ class ChartGenerator:
         )
         return pie
 
-    def create_wordcloud(self, words, title="Word Cloud") -> WordCloud:
+    def create_wordcloud(self, words, title="词云") -> WordCloud:
         """Generate a word cloud."""
         wordcloud = (
             WordCloud()
